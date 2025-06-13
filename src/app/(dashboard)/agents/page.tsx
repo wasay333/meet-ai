f9b5310fa9f84f6297757ccf96e2f61b833e34e0
abdulwasay <abdulwasayhashmi983@gmail.com>
2025-06-13 10:12:38 +0500

    agents filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,17 +1,27 @@
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { auth } from "@/lib/auth";
+import { loadSearchParams } from "@/modules/agents/params";
 import { AgentListHeader } from "@/modules/agents/ui/components/agent-list-header";
 import { AgentsView } from "@/modules/agents/ui/view/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs";
 import React, { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-const page = async () => {
+
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+
+const page = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(searchParams);
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  void queryClient.prefetchQuery(
+    trpc.agents.getMany.queryOptions({ ...filters })
+  );
   const session = await auth.api.getSession({
     headers: await headers(),
   });
