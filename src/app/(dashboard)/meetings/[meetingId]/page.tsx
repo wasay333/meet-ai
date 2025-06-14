ac6561f0b2048b078508119a210bb3817083eaf3
abdulwasay <abdulwasayhashmi983@gmail.com>
2025-06-14 18:49:46 +0500

    meetings setup

---
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>meeting id page</div>;
+};
+
+export default page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,8 @@
 import { agentsRouter } from "@/modules/agents/server/procedures";
 import { createTRPCRouter } from "../init";
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetingsRouter,
 });
 export type AppRouter = typeof appRouter;
